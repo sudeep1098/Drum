ca79c782e2f6e3ca5e8fafcc3309645942248452
Sudeep das <89259728+sudeep1098@users.noreply.github.com>
2023-04-08 16:06:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,31 +25,31 @@ function btnanime(currentkey) {
 function makesound(key) {
   switch (key) {
     case "w":
-      var sound = new Audio("/sounds/tom-1.mp3");
+      var sound = new Audio("/Drum/sounds/tom-1.mp3");
       sound.play();
       break;
     case "a":
-      var sound = new Audio("/sounds/tom-2.mp3");
+      var sound = new Audio("/Drum/sounds/tom-2.mp3");
       sound.play();
       break;
     case "s":
-      var sound = new Audio("/sounds/tom-3.mp3");
+      var sound = new Audio("/Drum/sounds/tom-3.mp3");
       sound.play();
       break;
     case "d":
-      var sound = new Audio("/sounds/tom-4.mp3");
+      var sound = new Audio("/Drum/sounds/tom-4.mp3");
       sound.play();
       break;
     case "j":
-      var sound = new Audio("/sounds/snare.mp3");
+      var sound = new Audio("/Drum/sounds/snare.mp3");
       sound.play();
       break;
     case "k":
-      var sound = new Audio("/sounds/crash.mp3");
+      var sound = new Audio("/Drum/sounds/crash.mp3");
       sound.play();
       break;
     case "l":
-      var sound = new Audio("/sounds/kick-bass.mp3");
+      var sound = new Audio("/Drum/sounds/kick-bass.mp3");
       sound.play();
       break;
 
